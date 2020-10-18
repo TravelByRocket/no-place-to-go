cb501fd66a06f1a605478e2f8a9df42d76ce9271
Bryan Costanza <bryan.costanza@colorado.edu>
2020-10-18 10:33:29 -0600

    Endless changes 3

--- apple/NoPlaceToGo/NoPlaceToGo/LamarA.swift
+++ apple/NoPlaceToGo/NoPlaceToGo/LamarA.swift
@@ -1,5 +1,5 @@
 //
-//  LamarA.swift
+//  LamarB.swift
 //  NoPlaceToGo
 //
 //  Created by Bryan Costanza on 8/27/20.
@@ -8,58 +8,27 @@
 import SwiftUI
 
 struct LamarA: View {
-    let site = AddressedSite.lamarA
-    
+    @State private var installIndex = 0
+    private var numInstallsAtSite = 3
     @EnvironmentObject private var pm: ProgressManager
     
     var body: some View {
-        NavigationView {
-            VStack {
-                
-                List {
-                    Text("Site: \(site.name)")
-                    Button(action: {
-                        self.site.getDirections()
-                    }) {
-                        Text("Get directions OPENS EXTERNAL MAP")
-                    }
-                    
-                    Button(action: {
-                        print("nothing yet")
-                    }) {
-                        Text("I have arrived ACTIVATES EXPERIENCE")
-                    }
-                    
-                    // more elegant solutions didn't work but this seems fine
-//                    ForEach (0 ..< site.artists.count) {i in
-//                        Text(self.site.artists[i].rawValue)
-//                    }
-                    
-                    NavigationLink(destination: StevenFrost()) {
-                        Text("Steven — Audio played before seeing his piece")
-                        
-                    }
-                    NavigationLink(destination: ChrissyGrace()) {
-                        Text("Chrissy and Grace — App “crashes”, plays background sound, Maybe 3-minute countdown timer to when the app comes back. ")
-                    }
-                    
-                    NavigationLink(destination: BAGBAYSHA()) {
-                        Text("Thomas, Koko, Chris — Simple additional information to installation")
-                    }
-                    
-                    Button(action: {
-                        self.pm.setLocation(to: self.site.nextLocation)
-                    }) {
-                        Text("Go to next site")
-                    }
-                }
+        VStack{
+            if installIndex == 0 {
+                NicoleBanowetz(installIndex: $installIndex, numInstallsAtSite: numInstallsAtSite)
+            } else if (installIndex == 1) {
+                NolanTredway()
+                NextInstallationButton(installIndex: $installIndex, numInstallsAtSite: numInstallsAtSite)
+            } else {
+                NextSitePage()
             }
+//            NextInstallationButton(installIndex: $installIndex, numInstallsAtSite: numInstallsAtSite)
         }
     }
 }
 
-struct LamarA_Previews: PreviewProvider {
+struct LamarB_Previews: PreviewProvider {
     static var previews: some View {
-        LamarA()
+        LamarB()
     }
 }
